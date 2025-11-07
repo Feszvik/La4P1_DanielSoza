72101982b0af8d655e73a74f9fd7d80ed0ade71e
Feszvik <daniel.sozasilva05@gmail.com>
2025-11-07 13:07:45 -0600

    Creando

---
+++ src/lab4p1_danielsoza/Lab4P1_DanielSoza.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package lab4p1_danielsoza;
+
+/**
+ *
+ * @author Daniel_Soza
+ */
+public class Lab4P1_DanielSoza {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
